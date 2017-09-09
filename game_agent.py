f521afc9e0b3897ff249cac7370c22f4ebbdf5cc
Ilya Semenov <semenovio@gmail.com>
2017-09-10 01:33:22 +0300

    Everything implemented and tested

--- game_agent.py
+++ game_agent.py
@@ -33,8 +33,15 @@ def custom_score(game, player):
         The heuristic value of the current game state to the specified player.
     """
     # DONE: finish this function!
-    return float(len(game.get_legal_moves()))
-    #raise NotImplementedError
+    if game.is_loser(player):
+        return float("-inf")
+
+    if game.is_winner(player):
+        return float("inf")
+
+    own_moves = len(game.get_legal_moves(player))
+    opp_moves = len(game.get_legal_moves(game.get_opponent(player)))
+    return float(own_moves - opp_moves)
 
 
 def custom_score_2(game, player):
@@ -59,6 +66,11 @@ def custom_score_2(game, player):
     float
         The heuristic value of the current game state to the specified player.
     """
+    if game.is_loser(player):
+        return float("-inf")
+
+    if game.is_winner(player):
+        return float("inf")
     # DONE: finish this function!
     return float(len(game.get_legal_moves()))
 
@@ -85,8 +97,15 @@ def custom_score_3(game, player):
     float
         The heuristic value of the current game state to the specified player.
     """
-    # DONE: finish this function!
-    return float(len(game.get_legal_moves()))
+    if game.is_loser(player):
+        return float("-inf")
+
+    if game.is_winner(player):
+        return float("inf")
+
+    own_moves = len(game.get_legal_moves(player))
+    opp_moves = len(game.get_legal_moves(game.get_opponent(player)))
+    return float(own_moves - opp_moves)
 
 class IsolationPlayer:
     """Base class for minimax and alphabeta agents -- this class is never
@@ -256,6 +275,8 @@ class MinimaxPlayer(IsolationPlayer):
 
         # DONE: finish this function!
         moves_list = game.get_legal_moves()
+        if len(moves_list) == 0:
+            return (-1, -1)
         maximum = -float("Inf")
         max_index = 0
         for i in range(len(moves_list)):
